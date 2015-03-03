8869a06c95e9518a498b0396ae315887f36465b1
catabriga <jhonatancatabriga@gmail.com>
2015-03-03 11:17:22 -0300

    adiciona variavel de ambiente para ip do servidor

--- servidor.js
+++ servidor.js
@@ -2,6 +2,9 @@ var servico = require('./servico'),
 	http = require('http'),
 	app = servico();
 
-http.createServer(app).listen(app.get('porta'), function () {
+var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port    = parseInt(process.env.OPENSHIFT_NODEJS_PORT) || 8080;
+
+http.createServer(app).listen(port, ipaddr, function () {
 	console.log('Aplicação escutando na porta', app.get('porta'));
 });
