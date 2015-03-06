34e57718cf7c78b667a95768568a8a588f7d9158
catabriga <jhonatancatabriga@gmail.com>
2015-03-06 11:14:37 -0300

    cria relatorios

--- cliente/app/aluno/alunoLista.controlador.js
+++ cliente/app/aluno/alunoLista.controlador.js
@@ -9,6 +9,8 @@
     this.pequisar = pequisar;
     this.remover = remover;
     this.limpar = limpar;
+    this.imprimir = imprimir;
+    this.marcarItens = marcarItens;
     deserializarPesquisa();
     buscar();
     buscarProfessores();
@@ -22,6 +24,55 @@
           $location.search(self.pesquisa)
           self.alunos = alunos
         });
+    }
+
+    function marcarItens() {
+      self.alunos.forEach(function (aluno) {
+        aluno.marcado = self.todos;
+      })
+    }
+
+    function imprimir() {
+      var doc = new jsPDF();
+      var linha = 10;
+      var nome = prompt("Escolha o nome para o relatório:");
+
+      if(!nome) {
+        toastr.warning('Nome do relatório é obrigatporio');
+        return;
+      }
+
+      if (!self.alunos) {
+        return;
+      }
+
+      var alunosMarcados = self.alunos.filter(function (aluno) {
+        return aluno.marcado;
+      });
+
+      if (alunosMarcados.length == 0) {
+        toastr.warning('Relatório não possui registros');
+        return;
+      }
+
+      var alunos = _.sortBy(alunosMarcados, function (aluno) {
+        return aluno.nome;
+      });
+
+      doc.setFontSize(22);
+      doc.text(10, linha += 10, nome);
+      doc.setFontSize(12);
+      alunos.forEach(function (aluno) {
+        var documento = aluno.cpf ? ',cpf: '+ aluno.cpf : ',rg: '+ aluno.rg;
+        doc.text(10, linha += 10,aluno.nome + documento);
+
+        if (linha % 280 == 0) {
+          doc.addPage();
+          linha = 10;
+        }
+      });
+
+      doc.save(nome);
     }
 
     function pequisar() {
